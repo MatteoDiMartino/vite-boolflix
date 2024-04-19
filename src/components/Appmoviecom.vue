a25c107a00c7022c3f0b0ca4cff2fba9c9275000
MatteoDiMartino <matteodimartino359@gmail.com>
2024-04-19 03:46:42 +0200

    feat: add base div to import information from API

--- src/components/Appmoviecom.vue
+++ src/components/Appmoviecom.vue
@@ -8,6 +8,14 @@ export default {
 
 <template>
 
+<div class="container">
+    <div class="row">
+        <div class="ms-titolo">[Titolo]</div>
+        <div class="ms-or-titolo">[Titolo Originale]</div>
+        <div class="ms-lingua">[Lingua]</div>
+        <div class="ms-voto">[Voto]</div>
+    </div>
+</div>
 
 </template>
 
