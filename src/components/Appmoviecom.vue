31dbc3fa8627686675ce0664435fe3eaa5e0145b
MatteoDiMartino <matteodimartino359@gmail.com>
2024-04-21 03:01:06 +0200

    feat: add original title

--- src/components/Appmoviecom.vue
+++ src/components/Appmoviecom.vue
@@ -14,7 +14,7 @@ export default {
 <div class="container">
     <div class="row">
         <div class="ms-titolo">Titolo: {{ cardInfo.title }} </div>
-        <div class="ms-or-titolo">[Titolo Originale]</div>
+        <div class="ms-or-titolo">Titolo Originale: {{ cardInfo.original_title }}</div>
         <div class="ms-lingua">[Lingua]</div>
         <div class="ms-voto">[Voto]</div>
     </div>
