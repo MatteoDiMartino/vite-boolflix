0dae822ae8d3075cd638e8fc00e1a4424c8bae0e
MatteoDiMartino <matteodimartino359@gmail.com>
2024-04-21 03:04:05 +0200

    fix: add vote average

--- src/components/Appmoviecom.vue
+++ src/components/Appmoviecom.vue
@@ -13,10 +13,12 @@ export default {
 
 <div class="container">
     <div class="row">
+        <img src="" alt="">
+
         <div class="ms-titolo">Titolo: {{ cardInfo.title }} </div>
         <div class="ms-or-titolo">Titolo Originale: {{ cardInfo.original_title }}</div>
         <div class="ms-lingua">Lingua Originale: {{ cardInfo.original_language }}</div>
-        <div class="ms-voto">Voto: {{ cardInfo.vote.average }}</div>
+        <div class="ms-voto">Voto: {{ cardInfo.vote_average }}</div>
     </div>
 </div>
 
