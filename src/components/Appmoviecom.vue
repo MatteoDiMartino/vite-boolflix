1f30b8989320b454f369e857731447ff3da1ec88
MatteoDiMartino <matteodimartino359@gmail.com>
2024-04-21 03:04:47 +0200

    feat: add new div ms-card

--- src/components/Appmoviecom.vue
+++ src/components/Appmoviecom.vue
@@ -13,12 +13,13 @@ export default {
 
 <div class="container">
     <div class="row">
-        <img src="" alt="">
-
-        <div class="ms-titolo">Titolo: {{ cardInfo.title }} </div>
-        <div class="ms-or-titolo">Titolo Originale: {{ cardInfo.original_title }}</div>
-        <div class="ms-lingua">Lingua Originale: {{ cardInfo.original_language }}</div>
-        <div class="ms-voto">Voto: {{ cardInfo.vote_average }}</div>
+        <div class="ms-card">
+            <img src="" alt="">
+            <div class="ms-titolo">Titolo: {{ cardInfo.title }} </div>
+            <div class="ms-or-titolo">Titolo Originale: {{ cardInfo.original_title }}</div>
+            <div class="ms-lingua">Lingua Originale: {{ cardInfo.original_language }}</div>
+            <div class="ms-voto">Voto: {{ cardInfo.vote_average }}</div>
+        </div>
     </div>
 </div>
 
