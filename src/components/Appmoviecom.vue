f24472cbe88e4428a5cc728d0e3f3637c05fa1a9
MatteoDiMartino <matteodimartino359@gmail.com>
2024-04-21 03:01:37 +0200

    feat: add original language

--- src/components/Appmoviecom.vue
+++ src/components/Appmoviecom.vue
@@ -15,7 +15,7 @@ export default {
     <div class="row">
         <div class="ms-titolo">Titolo: {{ cardInfo.title }} </div>
         <div class="ms-or-titolo">Titolo Originale: {{ cardInfo.original_title }}</div>
-        <div class="ms-lingua">[Lingua]</div>
+        <div class="ms-lingua">Lingua: {{ cardInfo.original_language }}</div>
         <div class="ms-voto">[Voto]</div>
     </div>
 </div>
